49f60ca7dfa12c2cb1ae20631cc488c7ac5e4e89
Bill Kable <bkable@gmail.com>
2020-03-23 15:43:41 -0500

    start point for vault-config-server lab

--- components/rest-support/src/main/java/io/pivotal/pal/tracker/restsupport/RestConfig.java
+++ components/rest-support/src/main/java/io/pivotal/pal/tracker/restsupport/RestConfig.java
@@ -1,21 +1,25 @@
 package io.pivotal.pal.tracker.restsupport;
 
-import org.springframework.boot.web.client.RestTemplateBuilder;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestOperations;
+import org.springframework.web.client.RestTemplate;
 
-import java.time.Duration;
 
 @Configuration
 public class RestConfig {
 
     @Bean
-    public RestOperations restOperations(RestOperationConfigProperties properties) {
-        return new RestTemplateBuilder()
-                .setReadTimeout(Duration.ofMillis(properties.getReadTimeoutMs()))
-                .setConnectTimeout(Duration.ofMillis(properties.getConnectTimeoutMs()))
-                .build();
+    public RestOperations restOperations() {
+        return new RestTemplate();
     }
 
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        return mapper;
+    }
 }
